82a5ccf4e3f268ec5409d307701fc42953805dfe
paulo091964 <pl840489307@gmail.com>
2026-01-16 15:11:51 -0300

    Atualizar o Poslxqt.sh

--- Poslxqt.sh
+++ Poslxqt.sh
@@ -4,13 +4,14 @@ set -e
 USUARIO="paulo"
 HOME_USUARIO="/home/$USUARIO"
 
-echo "========================================"
-echo " Pós-instalação Debian 13 Lxqt-core   "
-echo "========================================"
+echo "================================="
+echo " Pós-inst Debian 13 Lxqt-core   "
+echo "================================="
 
-# --------------------------------------------------
+# --------------------------------------
 # VERIFICAÇÕES BÁSICAS
-# --------------------------------------------------
+# --------------------------------------
+
 if [ "$EUID" -ne 0 ]; then
   echo "Execute como root."
   exit 1
@@ -21,38 +22,42 @@ if ! id "$USUARIO" >/dev/null 2>&1; then
   exit 1
 fi
 
-# --------------------------------------------------
+# -------------------------------------
 # UPDATE & BASE
-# --------------------------------------------------
+# -------------------------------------
+
 echo "==> Atualizando sistema"
 apt update && apt upgrade -y
 
-# --------------------------------------------------
-# BASE GRÁFICA LXQt
-# --------------------------------------------------
+# --------------------------------------
+# BASE GRÁFICA LXQT
+# --------------------------------------
+
 echo "==> Instalando Xorg + LXQt mínimo"
 
-apt install -y xorg lxqt-core lxqt-panel lxqt-session lxqt-config \
-  openbox lightdm dbus-x11 gvfs gvfs-backends gvfs-fuse x11-xserver-utils || true
+apt install -y xserver-xorg-core xinit \ lxqt-core lxqt-panel lxqt-session \ lxqt-config openbox dbus-x11 gvfs \ gvfs-backends gvfs-fuse \ x11-xserver-utils || true
 
-# --------------------------------------------------
+apt-get install lightdm \ --no-install-recomends -y
+
+# -------------------------------------
 # INTERNET, WI-FI E FIRMWARE
-# --------------------------------------------------
+#---------------------------------------
+
 echo "==> Instalando rede e firmwares"
 
 apt install -y network-manager network-manager-gnome wpasupplicant 
-  firmware-linux firmware-misc-nonfree firmware-realtek || true
+  firmware-linux firmware-misc-nonfree || true
 
-systemctl enable NetworkManager
+systemctl enable-now NetworkManager
 
-# --------------------------------------------------
-# --------------------------------------------------
+# ------------------------------------
 # PROGRAMAS ESSENCIAIS
-# --------------------------------------------------
+#-------------------------------------
+
 echo "==> Instalando programas essenciais"
 
-apt install -y mupdf htop abiword lximage-qt audacious xarchiver rclone \
-  falkon claws-mail transmission-qt pavucontrol gkrellm || true
+apt install -y mupdf htop abiword \ audacious xarchiver rclone \
+falkon claws-mail claws-mail-plugins \ pavucontrol gkrellm fonts-ubuntu || true
   
 echo" ==> Criar diretório conf gkrellm ,baixar themas copliland e descompactar na pasta ~/.gkrellm2/themes/
 
@@ -60,28 +65,30 @@ mkdir -p ~/.gkrellm2/themes/
 wget -c --progress=bar  http://www.muhri.net/gkrellm/CoplandOS.gkrellm.tar.gz
 tar -xvf CoplandOS.tar.gz -C ~/.gkrellm2/themes/
 
-# --------------------------------------------------
+# ------------------------------------
 # REMOÇÃO DE PROGRAMAS INÚTEIS
-# --------------------------------------------------
+# -------------------------------------
 
 
-# --------------------------------------------------
+# -------------------------------------
 # TERMINAIS E AUTOCOMPLETE
-# --------------------------------------------------
+# -------------------------------------
+
 echo "==> Instalando terminais e bash completion"
 
-apt install -y lxterminal qterminal bash-completion fonts-dejavu-core || true
+apt install -y lxterminal qterminal bash-completion fonts-ubuntu || true
 
-# --------------------------------------------------
+# -------------------------------------
 # QTERMINAL - CONFIGURAÇÃO
-# --------------------------------------------------
+# ------------------------------------
+
 echo "==> Configurando lxterminal"
 
 mkdir -p "$HOME_USUARIO/.config/qterminal.org"
 
 cat > "$HOME_USUARIO/.config/qterminal.org/qterminal.ini" <<'EOF'
 [General]
-Font=DejaVu Sans Mono,11,-1,5,50,0,0,0,0,0
+Font=Ubuntu Mono,14,-1,5,50,0,0,0,0,0
 ColorScheme=Linux
 ScrollbarPosition=Hidden
 HistoryLimited=true
@@ -92,14 +99,15 @@ chown -R "$USUARIO:$USUARIO" "$HOME_USUARIO/.config"
 
 update-alternatives --set x-terminal-emulator /usr/bin/qterminal
 
-# --------------------------------------------------
+# ------------------------------------
 # XTERM (FALLBACK)
-# --------------------------------------------------
+# -----------------------------------
+
 echo "==> Configurando xterm"
 
 cat > "$HOME_USUARIO/.Xresources" <<'EOF'
-XTerm*faceName: DejaVu Sans Mono
-XTerm*faceSize: 11
+XTerm*faceName: Ubuntu mono 
+XTerm*faceSize: 14
 XTerm*foreground: green
 XTerm*background: black
 XTerm*cursorColor: green
@@ -118,9 +126,10 @@ EOF
 
 chmod +x /etc/X11/Xsession.d/90xresources
 
-# --------------------------------------------------
+# ------------------------------------
 # BASH COMPLETION GLOBAL
-# --------------------------------------------------
+# ------------------------------------
+
 if ! grep -q bash_completion /etc/bash.bashrc; then
 cat >> /etc/bash.bashrc <<'EOF'
 
@@ -130,9 +139,10 @@ fi
 EOF
 fi
 
-# --------------------------------------------------
+# --------------------------------------
 # LOGIN AUTOMÁTICO
-# --------------------------------------------------
+# -------------------------------------
+
 echo "==> Configurando login automático"
 
 mkdir -p /etc/lightdm/lightdm.conf.d
@@ -144,14 +154,16 @@ autologin-user-timeout=0
 user-session=lxqt
 EOF
 
-# --------------------------------------------------
+# -------------------------------------
 # GRUPOS DO USUÁRIO
-# --------------------------------------------------
+# -------------------------------------
+
 usermod -aG audio,video,netdev,plugdev "$USUARIO"
 
-# --------------------------------------------------
+# ------------------------------------
 # SWAP EM ARQUIVO
-# --------------------------------------------------
+# ------------------------------------
+
 echo "==> Configurando swapfile"
 
 if ! swapon --show | grep -q "/swapfile"; then
@@ -165,9 +177,10 @@ if ! grep -q "/swapfile" /etc/fstab; then
   echo "/swapfile none swap sw 0 0" >> /etc/fstab
 fi
 
-# --------------------------------------------------
+# ------------------------------------
 # OTIMIZAÇÕES
-# --------------------------------------------------
+# -----------------------------------
+
 echo "==> Aplicando otimizações"
 
 echo "vm.swappiness=10" > /etc/sysctl.d/99-swappiness.conf
@@ -183,22 +196,24 @@ EOF
 
 systemctl restart systemd-journald
 
-# --------------------------------------------------
+# -----------------------------------
 # DISPLAY MANAGER
-# --------------------------------------------------
+# ------------------------------------
+
 systemctl enable lightdm
 
-# --------------------------------------------------
+# -----------------------------------
 # LIMPEZA FINAL
-# --------------------------------------------------
+# -----------------------------------
+
 echo "==> Limpeza final"
 
 apt autoremove -y
 apt clean
 
-echo "========================================"
+echo "================================="
 echo " Sistema pronto."
 echo " Reiniciando..."
-echo "========================================"
+echo "================================="
 
 reboot
